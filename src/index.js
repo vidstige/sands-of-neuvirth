52e01d56e8bdc59aeed9db4c1dc53030e83e2c2a
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-04-09 17:32:59 +1200

    receive data from backend

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 require("../style.css");
 
+const consume = responseReader => {
+    return responseReader.read().then(result => {
+        if (result.done) { return; }
+
+        // do something with the current chunk
+        const chunk = result.value;
+        console.log(chunk.length);
+
+        return consume(responseReader);
+    });
+}
+
+// Perform the request and consume response stream
+
 function load() {
-    console.log('loaded');
+    const url = '/api/sand';
+    fetch(url).then(response => {
+        return consume(response.body.getReader());
+    })
+    .catch(console.error);
 }
 
 document.addEventListener("DOMContentLoaded", load);
--- src/server.js
+++ src/server.js
@@ -2,17 +2,23 @@ const express = require('express');
 const webpack = require('webpack');
 const middleware = require('webpack-dev-middleware'); //webpack hot reloading middleware
 
+var spawn = require('child_process').spawn;
+
 const cfg = require('../webpack.config.js');
 const compiler = webpack(cfg);
 
 const app = express();
 
-app.get('/hello', function (req, res) {
-  res.send('Hello World!');
+app.get('/api/sand', function (req, res) {
+  //spawn('cat', ['/dev/urandom']).stdout.pipe(res);
+  //res.send('hi');
+  setInterval(function() {
+    res.write('1234567890');
+  }, 250);
 });
 
 app.use(middleware(compiler, {
   // webpack-dev-middleware options
 }));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
